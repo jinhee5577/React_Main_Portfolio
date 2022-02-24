453f452abe2d11fe4d48660f2e53675daee20653
jinhee oh <exojj.com.gmail.com>
2022-02-24 16:04:20 +0900

    add a project list

--- src/data.js
+++ src/data.js
@@ -29,6 +29,14 @@ export default [
                homelink : 'https://jinhee5577.github.io/Jinhee_PF/'        
             },
             {  id : 4,
+               explanation : <p><span>React, Localstorage, Redux</span>로 개발된 <span>야나두 TODO_LIST</span> 웹페이지 입니다.<br/>
+                            이 프로젝트는 모바일에서 보기 적합하도록 <span>max-width : 600px 반응형 웹사이트</span>로 개발하였습니다.<br/><br/>
+                            작성된 TODO_LIST data를 <span>Localstorage</span>를 이용하여, <span> CRUD기능개발 과 영구저장</span>이 가능합니다. 
+                            추가로 Lotto번호를 뽑아주는 페이지를 <span>Javascript와 Redux</span>로 개발하여 페이지 이동간에도 lotto번호 정보가 유효합니다. </p>,
+               gitlink : 'https://github.com/jinhee5577/TODO_LIST',     
+               homelink : 'https://jinhee5577.github.io/YANADO/'        
+            },
+            {  id : 5,
                explanation : <p><span>NAUM_mall</span> 프로젝트는 제가 그전회사에서 마지막으로 맡은 쇼핑몰 리뉴얼프로젝트 입니다.<br/><br/>
                               목포에서 만큼은 가장 세련된 쇼핑몰 사이트 가 나올수있도록 대표 메이저 백화점 사이트를 참고하였습니다.<br/> 
                               웹,모바일 버전의 각각의 메인 페이지부터 서브페이지까지 <span>디자인과 퍼블리싱 작업</span>을 담당했습니다.<br/><br/>                                                         
@@ -37,7 +45,7 @@ export default [
                gitlink : 'https://github.com/jinhee5577/jinhee5577.github.io',     
                homelink : 'https://jinhee5577.github.io/A이걸봐주세요_naum_moblie.html'        
             },
-            {  id : 5,
+            {  id : 6,
                explanation : <p><br/><span>SoundBean 카페</span> 프로젝트는 제가 그전회사에서 디자인과 퍼블리싱한 프로젝트중 마음에 드는 결과물중 하나입니다.<br/><br/> 
                               유명 카페브랜드 사이트들을 참고하여 PC와 모바일에서 각각적합한 <span>반응형 홈페이지</span> 형태로 제작되었습니다.<br/>
                               각각의 메인 페이지부터 서브페이지까지 <span>디자인과 퍼블리싱 작업</span>을 담당했습니다.<br/>                             
