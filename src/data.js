c74861d4ac65ce14dc649e829e11b68ff6693c9c
jinhee oh <exojj.com.gmail.com>
2022-07-24 19:32:37 +0900

    add new react hansan project

--- src/data.js
+++ src/data.js
@@ -7,7 +7,7 @@ export default [
                             그외 더보기상품은 <span>axios</span>로 요청 하여 불러옵니다.<br/><br/>
                             <span>Localstorage</span>를 이용하여 최근본 상품의 정보를 3개까지만 중복제거하여 이미지가 추가됩니다.<br/>
                             <span>그외에도 몇가지 기능개발이 더있습니다. 다음 박스에서 설명 드리겠습니다.</span></p>,
-               gitlink : 'https://github.com/jinhee5577/JHreact',     
+               gitlink : 'https://github.com/jinhee5577/JHreact/tree/master/src',     
                homelink : 'https://jinhee5577.github.io/reactJH/'        
             },
             {  id : 2,
@@ -26,19 +26,27 @@ export default [
                              PC와 모바일에서 보기 적합하도록 <span>반응형 웹사이트</span>로 제작하였습니다.<br/><br/>
                              저를 소개하는 포토폴리오 인만큼 좀더 근사하고, 다양한 <span>animation</span>과 <span>인터렉티브</span>한 웹페이지로 
                              제작하기 위해<span>CSS</span>와 <span>Javascript</span>로 직접 개발하였습니다. </p>,
-               gitlink : 'https://github.com/jinhee5577/Main_Portfolio',     
+               gitlink : 'https://github.com/jinhee5577/Main_Portfolio/tree/master/src',     
                homelink : 'https://jinhee5577.github.io/Jinhee_PF/'         
             },
             {  id : 4,
+               explanation : <p><span>React</span> 와 <span>Javascript</span> 를 이용하여 <span>영화 한산, 한산도대첩</span>을 모티브로 개발한 <span>웹 홍보사이트</span> 입니다.<br/><br/>
+                             역동적인 홍보페이지를 구현하기 위해 animation 라이브러리를 전혀 이용하지 않고 <span>순수javascript</span>를 이용하여 다양한 
+                             <span>animation과 판옥선의 학익진대형, 발포, D-day 날짜,시간</span> 구현 등을 개발 완성했습니다.<br/><br/>
+                             영화상세 데이터와, 각배우별 상세데이터는 <span>영화진흥위원회의 오픈API</span>를 <span>axios</span>로 요청하여 <span>각각 조건에 맞는 정보</span>를 렌더링 해줍니다. </p>,
+               gitlink : 'https://github.com/jinhee5577/Hansan_code/tree/master/src',     
+               homelink : 'https://jinhee5577.github.io/Hansan/'         
+            },
+            {  id : 5,
                explanation : <p><span>React, Localstorage, Redux</span>로 개발된 <span>야나두 TODO_LIST</span> 웹페이지 입니다.<br/>
                             이 프로젝트는 모바일에서 보기 적합하도록 <span>max-width : 600px 반응형 웹사이트</span>로 개발하였습니다.<br/><br/>
                             작성된 TODO_LIST data를 <span>Localstorage</span>를 이용하여, <span> CRUD기능개발 과 영구저장</span>이 가능합니다.<br/>
                             추가로 Lotto번호를 뽑아주는 페이지를 <span>Javascript와 Redux</span>를 이용하여 개발하였고, 페이지 이동간에도 
                             lotto번호 정보가 유효합니다. </p>,
-               gitlink : 'https://github.com/jinhee5577/TODO_LIST',     
+               gitlink : 'https://github.com/jinhee5577/TODO_LIST/tree/master/src',     
                homelink : 'https://jinhee5577.github.io/YANADO/'        
             },
-            {  id : 5,
+            {  id : 6,
                explanation : <p><span>NAUM_mall</span> 리뉴얼 프로젝트는 제가 회사에서 마지막으로 담당한 쇼핑몰 리뉴얼 프로젝트 입니다.<br/><br/>
                               목포에서 만큼은 가장 세련된 쇼핑몰 사이트 가 나올수있도록 대표 메이저 백화점 사이트를 참고하였습니다.<br/> 
                               웹,모바일 버전의 각각의 메인 페이지부터 서브페이지까지 <span>디자인과 퍼블리싱 작업</span>을 담당했습니다.<br/><br/>                                                         
@@ -47,7 +55,7 @@ export default [
                gitlink : 'https://github.com/jinhee5577/jinhee5577.github.io',     
                homelink : 'https://jinhee5577.github.io/A이걸봐주세요_naum_moblie.html'        
             },
-            {  id : 6,
+            {  id : 7,
                explanation : <p><br/><span>SoundBean 카페</span> 프로젝트는 제가 회사에서 디자인과 퍼블리싱을 담당한 프로젝트중 마음에 드는 결과물중 하나 입니다.<br/><br/> 
                               이 프로젝트는 유명 카페브랜드 사이트들을 참고하여 PC와 모바일에서 각각 적합한 <span>반응형 홈페이지</span> 형태로 제작되었습니다.<br/><br/>
                               각각의 메인 페이지부터 서브페이지까지 <span>디자인과 퍼블리싱 작업</span>을 담당하였고, <span>총 3페이지</span>의 카페 웹사이트 입니다.<br/>                             
