ca8baf27446b3bf728b843c5e7ea390261fb196d
ohjinhee <riricos153@naver.com>
2023-02-01 23:51:51 +0900

    새로운 프로젝트 추가

--- src/data.js
+++ src/data.js
@@ -1,69 +1,69 @@
 export default [
-            {  id : 1,
-               explanation : <p><span>JH VIP</span> 프로젝트는 제가 그동안 react.js의 유료 온라인강의를
-                            수강하면서 공부한 모든 내용들이 들어간 총 집합체 입니다. <br/><br/>
-                            <span>React</span> 로 개발된 웹페이지이며, <span>Redux</span>를 사용하여 쇼핑몰에서 장바구니페이지 의
-                            기본적인 <span>CRUD</span>기능과 중복 상품은 추가되지 않도록 기능개발 되었습니다. 
-                            그외 더보기상품은 <span>axios</span>로 요청 하여 불러옵니다.<br/><br/>
-                            <span>Localstorage</span>를 이용하여 최근본 상품의 정보를 3개까지만 중복제거하여 이미지가 추가됩니다.<br/>
-                            <span>그외에도 몇가지 기능개발이 더있습니다. 다음 박스에서 설명 드리겠습니다.</span></p>,
-               gitlink : 'https://github.com/jinhee5577/JHreact/tree/master/src',     
-               homelink : 'https://jinhee5577.github.io/reactJH/'        
-            },
-            {  id : 2,
-               explanation : <p><span>영화API</span>를 요청하여 인기영화 20위를 구현하였고, 해당영화의 상세페이지,
-                                예매하기 페이지를 각각 개발했습니다.<br/>
-                                특히 예매페이지의 2주간 달력과, 상영관 옵션정보 의 기능개발을 <span>Javascript</span> 로 직접개발 하였습니다.<br/><br/>
-                                추가로 <span>카카오지도 API</span>를 이용하여 
-                                검색장소의 리스트목록을 보여주도록 개발되었습니다.<br/>
-                                <span>로그인페이지 기능</span>은 간단하게 user정보를 JSON파일로 저장해두었고 <span>axios</span>로 요청하여 id와 비밀번호를 검사합니다.
-                                id와 비밀번호가 일치할경우만 회원멤버창이 뜨도록 간단하게 개발하였습니다.</p>,
-               gitlink : 'https://github.com/jinhee5577/JHreact/tree/master/src',     
-               homelink : 'https://jinhee5577.github.io/reactJH/'        
-            },
-            {  id : 3,
-               explanation : <p><span>React</span>로 개발된 <span>저의 메인 포토폴리오</span> 웹사이트 입니다.<br/>
-                             PC와 모바일에서 보기 적합하도록 <span>반응형 웹사이트</span>로 제작하였습니다.<br/><br/>
-                             저를 소개하는 포토폴리오 인만큼 좀더 근사하고, 다양한 <span>animation</span>과 <span>인터렉티브</span>한 웹페이지로 
-                             제작하기 위해<span>CSS</span>와 <span>Javascript</span>로 직접 개발하였습니다.<br/> 
-                             추가로 <span>play버튼</span>을 클릭시 <span>음악재생</span>과 함께 랜덤한 <span>아름다운 별빛</span>이 
-                             함께 쏟아지도록 개발하였습니다.</p>,
-               gitlink : 'https://github.com/jinhee5577/Main_Portfolio/tree/master/src',     
-               homelink : 'https://jinhee5577.github.io/Jinhee_PF/'         
-            },
-            {  id : 4,
-               explanation : <p><span>React</span> 와 <span>Javascript</span> 를 이용하여 <span>영화 한산, 한산도대첩</span>을 모티브로 개발한 <span>웹 홍보사이트</span> 입니다.<br/><br/>
-                             역동적인 홍보페이지를 구현하기 위해 animation 라이브러리를 전혀 이용하지 않고 <span>순수javascript</span>를 이용하여 다양한 
-                             <span>animation과 판옥선의 학익진대형, 발포, D-day 날짜,시간</span> 구현 등을 개발 완성했습니다.<br/><br/>
-                             영화상세 데이터와, 각배우별 상세데이터는 <span>영화진흥위원회의 오픈API</span>를 <span>axios</span>로 요청하여 <span>각각 조건에 맞는 정보</span>를 렌더링 해줍니다. </p>,
-               gitlink : 'https://github.com/jinhee5577/Hansan_code/tree/master/src',     
-               homelink : 'https://jinhee5577.github.io/Hansan/'         
-            },
-            {  id : 5,
-               explanation : <p><span>React, Localstorage, Redux</span>로 개발된 <span>야나두 TODO_LIST</span> 웹페이지 입니다.<br/>
-                            이 프로젝트는 모바일에서 보기 적합하도록 <span>max-width : 600px 반응형 웹사이트</span>로 개발하였습니다.<br/><br/>
-                            작성된 TODO_LIST data를 <span>Localstorage</span>를 이용하여, <span> CRUD기능개발 과 영구저장</span>이 가능합니다.<br/>
-                            추가로 Lotto번호를 뽑아주는 페이지를 <span>Javascript와 Redux</span>를 이용하여 개발하였고, 페이지 이동간에도 
-                            lotto번호 정보가 유효합니다. </p>,
-               gitlink : 'https://github.com/jinhee5577/TODO_LIST/tree/master/src',     
-               homelink : 'https://jinhee5577.github.io/YANADO/'        
-            },
-            {  id : 6,
-               explanation : <p><span>JINI3D</span> 프로젝트는 새로운시도로 웹메인페이지 내용을 <span>3D</span>로 구현하여 창의적이며 입체적인 UX를 선보이기위해 현재도 계속 개발진행중인 프로젝트입니다.<br/><br/>
-                              <span>3D구현</span>은 라이브러리를 전혀 이용하지않고, <span>React</span>와 순수<span>Javascript, CSS</span>를 이용하여 입체적인 메인페이지를 개발 완성했습니다.<br/> 
-                              또한 간단한 프로필페이지에서 선택한 이미지와,이름을 <span>Localstorage</span>에 영구적으로 저정하고 불러오며 회원처럼 보여줍니다.<br/><br/>                                                         
-                              <span>아파트매매가</span>조회는 프로필을 저장한 고객만 보이며, 데이터는 <span>국토교통부api</span>를 각지역 옵션창에서 선택한 지역을 해당법정동코드list에서 지역코드를 찾은후 해당지역 아파트매매정보를 요청합니다.
-                             </p>,
-               gitlink : 'https://github.com/jinhee5577/JINI3D',     
-               homelink : 'https://jinipro-a2903.web.app/'        
-            },
-            {  id : 7,
-               explanation : <p><br/><span>SoundBean 카페</span> 프로젝트는 제가 회사에서 디자인과 퍼블리싱을 담당한 프로젝트중 마음에 드는 결과물중 하나 입니다.<br/><br/> 
-                              이 프로젝트는 유명 카페브랜드 사이트들을 참고하여 PC와 모바일에서 각각 적합한 <span>반응형 홈페이지</span> 형태로 제작되었습니다.<br/><br/>
-                              각각의 메인 페이지부터 서브페이지까지 <span>디자인과 퍼블리싱 작업</span>을 담당하였고, <span>총 3페이지</span>의 카페 웹사이트 입니다.<br/>                             
-                             </p>,
-               gitlink : 'https://github.com/jinhee5577',     
-               homelink : 'http://soundbean.shop/'        
-            },          
-
-         ];
+      {  id : 1,
+         explanation : <p><span>JINI3D</span> 프로젝트는 새로운시도로 웹메인페이지 내용을 <span>3D</span>로 구현하여 창의적이며 입체적인 UX를 선보이기위해 현재도 계속 개발진행중인 프로젝트입니다.<br/><br/>
+                        <span>3D구현</span>은 라이브러리를 전혀 이용하지않고, <span>React</span>와 <span>Javascript,CSS</span>를 이용하여 입체적인 메인페이지를 개발 완성했습니다.
+                        또한 간단한 <span>프로필페이지</span>에서 선택한 이미지와,이름을 <span>Localstorage</span>에 영구적으로 저정하고 불러오며 회원처럼 보여줍니다.<br/><br/>                                                         
+                        <span>아파트매매가</span>조회는 프로필을 저장한 고객만 보이며, 데이터는 <span>국토교통부api</span>를 각지역 옵션창에서 선택한 지역을 법정동코드list에서 <span>지역코드</span>를 
+                        찾은후 해당지역 아파트매매정보를 요청합니다.
+                     </p>,
+         gitlink : 'https://github.com/jinhee5577/JINI3D',     
+         homelink : 'https://jinipro-a2903.web.app/'        
+      },
+      {  id : 2,
+         explanation : <p><span>JH VIP</span> 프로젝트는 제가 그동안 react.js의 유료 온라인강의를
+                        수강하면서 공부한 모든 내용들이 들어간 총 집합체 입니다. <br/><br/>
+                        <span>React</span> 로 개발된 웹페이지이며, <span>Redux</span>를 사용하여 쇼핑몰에서 장바구니페이지 의
+                        기본적인 <span>CRUD</span>기능과 중복 상품은 추가되지 않도록 기능개발 되었습니다. 
+                        그외 더보기상품은 <span>axios</span>로 요청 하여 불러옵니다.<br/><br/>
+                        <span>Localstorage</span>를 이용하여 최근본 상품의 정보를 3개까지만 중복제거하여 이미지가 추가됩니다.<br/>
+                        <span>그외에도 몇가지 기능개발이 더있습니다. 다음 박스에서 설명 드리겠습니다.</span></p>,
+         gitlink : 'https://github.com/jinhee5577/JHreact/tree/master/src',     
+         homelink : 'https://jinhee5577.github.io/reactJH/'        
+      },
+      {  id : 3,
+         explanation : <p><span>영화API</span>를 요청하여 인기영화 20위를 구현하였고, 해당영화의 상세페이지,
+                           예매하기 페이지를 각각 개발했습니다.<br/>
+                           특히 예매페이지의 2주간 달력과, 상영관 옵션정보 의 기능개발을 <span>Javascript</span> 로 직접개발 하였습니다.<br/><br/>
+                           추가로 <span>카카오지도 API</span>를 이용하여 
+                           검색장소의 리스트목록을 보여주도록 개발되었습니다.<br/>
+                           <span>로그인페이지 기능</span>은 간단하게 user정보를 JSON파일로 저장해두었고 <span>axios</span>로 요청하여 id와 비밀번호를 검사합니다.
+                           id와 비밀번호가 일치할경우만 회원멤버창이 뜨도록 간단하게 개발하였습니다.</p>,
+         gitlink : 'https://github.com/jinhee5577/JHreact/tree/master/src',     
+         homelink : 'https://jinhee5577.github.io/reactJH/'        
+      },
+      {  id : 4,
+         explanation : <p><span>React</span>로 개발된 <span>저의 메인 포토폴리오</span> 웹사이트 입니다.<br/>
+                        PC와 모바일에서 보기 적합하도록 <span>반응형 웹사이트</span>로 제작하였습니다.<br/><br/>
+                        저를 소개하는 포토폴리오 인만큼 좀더 근사하고, 다양한 <span>animation</span>과 <span>인터렉티브</span>한 웹페이지로 
+                        제작하기 위해<span>CSS</span>와 <span>Javascript</span>로 직접 개발하였습니다.<br/> 
+                        추가로 <span>play버튼</span>을 클릭시 <span>음악재생</span>과 함께 랜덤한 <span>아름다운 별빛</span>이 
+                        함께 쏟아지도록 개발하였습니다.</p>,
+         gitlink : 'https://github.com/jinhee5577/Main_Portfolio/tree/master/src',     
+         homelink : 'https://jinhee5577.github.io/Jinhee_PF/'         
+      },
+      {  id : 5,
+         explanation : <p><span>React</span> 와 <span>Javascript</span> 를 이용하여 <span>영화 한산, 한산도대첩</span>을 모티브로 개발한 <span>웹 홍보사이트</span> 입니다.<br/><br/>
+                        역동적인 홍보페이지를 구현하기 위해 animation 라이브러리를 전혀 이용하지 않고 <span>순수javascript</span>를 이용하여 다양한 
+                        <span>animation과 판옥선의 학익진대형, 발포, D-day 날짜,시간</span> 구현 등을 개발 완성했습니다.<br/><br/>
+                        영화상세 데이터와, 각배우별 상세데이터는 <span>영화진흥위원회의 오픈API</span>를 <span>axios</span>로 요청하여 <span>각각 조건에 맞는 정보</span>를 렌더링 해줍니다. </p>,
+         gitlink : 'https://github.com/jinhee5577/Hansan_code/tree/master/src',     
+         homelink : 'https://jinhee5577.github.io/Hansan/'         
+      },
+      {  id : 6,
+         explanation : <p><span>React, Localstorage, Redux</span>로 개발된 <span>야나두 TODO_LIST</span> 웹페이지 입니다.<br/>
+                        이 프로젝트는 모바일에서 보기 적합하도록 <span>max-width : 600px 반응형 웹사이트</span>로 개발하였습니다.<br/><br/>
+                        작성된 TODO_LIST data를 <span>Localstorage</span>를 이용하여, <span> CRUD기능개발 과 영구저장</span>이 가능합니다.<br/>
+                        추가로 Lotto번호를 뽑아주는 페이지를 <span>Javascript와 Redux</span>를 이용하여 개발하였고, 페이지 이동간에도 
+                        lotto번호 정보가 유효합니다. </p>,
+         gitlink : 'https://github.com/jinhee5577/TODO_LIST/tree/master/src',     
+         homelink : 'https://jinhee5577.github.io/YANADO/'        
+      },
+      {  id : 7,
+         explanation : <p><br/><span>SoundBean 카페</span> 프로젝트는 제가 회사에서 디자인과 퍼블리싱을 담당한 프로젝트중 마음에 드는 결과물중 하나 입니다.<br/><br/> 
+                        이 프로젝트는 유명 카페브랜드 사이트들을 참고하여 PC와 모바일에서 각각 적합한 <span>반응형 홈페이지</span> 형태로 제작되었습니다.<br/><br/>
+                        각각의 메인 페이지부터 서브페이지까지 <span>디자인과 퍼블리싱 작업</span>을 담당하였고, <span>총 3페이지</span>의 카페 웹사이트 입니다.<br/>                             
+                        </p>,
+         gitlink : 'https://github.com/jinhee5577',     
+         homelink : 'http://soundbean.shop/'        
+      },          
+];
