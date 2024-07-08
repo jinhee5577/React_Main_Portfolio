c5dd7a5aa142f2ab69c54060692b2dff5c925f9a
ohjinhee <riricos153@naver.com>
2024-07-08 19:06:19 +0900

    빅데이터 분석 개발자과정 프로젝트 추가

--- src/data.js
+++ src/data.js
@@ -1,5 +1,15 @@
 export default [
       {  id : 1,
+         explanation : <p><span>YOLORAY</span>프로젝트는 <span>딥러닝</span>을 이용한 공항 X-ray위해물품 <span>자동 탐지시스템</span> 웹서비스 입니다.<br/><br/>
+                        기존의 보안검색 과정을 <span>딥러닝 YOLOv8</span>을 활용하여 실시간 <span>X-ray 자동판독 솔루션</span> 서비스를 구축해 <span>높은 정확도</span>와
+                         <span>신속한 탐지</span>, <span>효율성</span> 향상, 보안 강화에 기여를 목적으로한 <span>대시보드</span>형태의 자동판독 웹서비스 입니다.<br/><br/>
+                        직관적 대시보드 설계를 통해 <span>자동탐지, 통계 데이터 모니터링</span> 및 <span>관리기능</span>을 제공합니다.<br/>
+                        <span>사용기술:</span> Spring, FastAPI, React, YOLOv8, Oracle, MyBatis, JAVA, Javascript, python, Slack, Jira, Apache, Github 입니다.
+                     </p>,
+         gitlink : 'https://github.com/jinhee5577/Airport_Xray_detection',     
+         homelink : 'https://github.com/jinhee5577/Airport_Xray_detection'        
+      },
+      {  id : 2,
          explanation : <p><span>JINI3D</span> 프로젝트는 새로운시도로 웹메인페이지 내용을 <span>3D</span>로 구현하여 창의적이며 입체적인 UX를 선보이기위해 현재도 계속 개발진행중인 프로젝트입니다.<br/><br/>
                         <span>3D구현</span>은 라이브러리를 전혀 이용하지않고, <span>React</span>와 <span>Javascript,CSS</span>를 이용하여 입체적인 메인페이지를 개발 완성했습니다.
                         또한 간단한 <span>프로필페이지</span>에서 선택한 이미지와,이름을 <span>Localstorage</span>에 영구적으로 저정하고 불러오며 회원처럼 보여줍니다.<br/><br/>                                                         
@@ -9,7 +19,19 @@ export default [
          gitlink : 'https://github.com/jinhee5577/JINI3D',     
          homelink : 'https://jinipro-a2903.web.app/'        
       },
-      {  id : 2,
+      {  id : 3,
+         explanation : <p><span>PLAY SCENT</span>프로젝트는 <span>프리미엄</span> 향수의 접근성과 고가의 향수 대중화를 높이는 전략으로
+                         간단한 설문조사로 얻은 고객별 <span>향수 취향 데이터</span>를 <span>코사인유사도 체크 과정</span>을 거쳐 <span>고객맞춤</span> 향수제품을 추천해 주어 주문한 상품의 
+                         <span>간편 결제기능</span> 까지 지원하는 전자상거래 서비스입니다.<br/><br/>
+                         <span>Portone</span>의 <span>결제api</span>를 연동하여 실제 주문 상품의 금액 <span>결제 기능</span>까지 직접 개발구현 하여 한층더 백엔드 스킬을 발전 할수 있던 기회였습니다.<br/><br/>
+                         또한 사용자분들에게 새로운 경험을 선사하기 위해 <span>메인 상품진열 UI</span>에 <span>Swiper병풍</span> 슬라이드와 <span>향기네컷</span> 페이지의 <span>SVG애니메이션</span> 
+                         적용으로 역동적인 포퍼먼스를 구현 하였습니다.<br/>
+                        <span>사용기술:</span> JAVA, Javascript,python,HTML5,CSS3, JSP,Servlet, Flask, Apache,MyBatis, Oracle, Github 입니다.
+                     </p>,
+         gitlink : 'https://github.com/jinhee5577/PLAY_SCENT',
+         homelink : 'https://github.com/jinhee5577/PLAY_SCENT'        
+      },
+      {  id : 4,
          explanation : <p><span>JH VIP</span> 프로젝트는 제가 그동안 react.js의 유료 온라인강의를
                         수강하면서 공부한 모든 내용들이 들어간 총 집합체 입니다. <br/><br/>
                         <span>React</span> 로 개발된 웹페이지이며, <span>Redux</span>를 사용하여 쇼핑몰에서 장바구니페이지 의
@@ -20,7 +42,7 @@ export default [
          gitlink : 'https://github.com/jinhee5577/JHreact/tree/master/src',     
          homelink : 'https://jinhee5577.github.io/reactJH/'        
       },
-      {  id : 3,
+      {  id : 5,
          explanation : <p><span>영화API</span>를 요청하여 인기영화 20위를 구현하였고, 해당영화의 상세페이지,
                            예매하기 페이지를 각각 개발했습니다.<br/>
                            특히 예매페이지의 2주간 달력과, 상영관 옵션정보 의 기능개발을 <span>Javascript</span> 로 직접개발 하였습니다.<br/><br/>
@@ -31,7 +53,7 @@ export default [
          gitlink : 'https://github.com/jinhee5577/JHreact/tree/master/src',     
          homelink : 'https://jinhee5577.github.io/reactJH/'        
       },
-      {  id : 4,
+      {  id : 6,
          explanation : <p><span>React</span>로 개발된 <span>저의 메인 포토폴리오</span> 웹사이트 입니다.<br/>
                         PC와 모바일에서 보기 적합하도록 <span>반응형 웹사이트</span>로 제작하였습니다.<br/><br/>
                         저를 소개하는 포토폴리오 인만큼 좀더 근사하고, 다양한 <span>animation</span>과 <span>인터렉티브</span>한 웹페이지로 
@@ -41,7 +63,7 @@ export default [
          gitlink : 'https://github.com/jinhee5577/Main_Portfolio/tree/master/src',     
          homelink : 'https://jinhee5577.github.io/Jinhee_PF/'         
       },
-      {  id : 5,
+      {  id : 7,
          explanation : <p><span>React</span> 와 <span>Javascript</span> 를 이용하여 <span>영화 한산, 한산도대첩</span>을 모티브로 개발한 <span>웹 홍보사이트</span> 입니다.<br/><br/>
                         역동적인 홍보페이지를 구현하기 위해 animation 라이브러리를 전혀 이용하지 않고 <span>순수javascript</span>를 이용하여 다양한 
                         <span>animation과 판옥선의 학익진대형, 발포, D-day 날짜,시간</span> 구현 등을 개발 완성했습니다.<br/><br/>
@@ -49,7 +71,7 @@ export default [
          gitlink : 'https://github.com/jinhee5577/Hansan_code/tree/master/src',     
          homelink : 'https://jinhee5577.github.io/Hansan/'         
       },
-      {  id : 6,
+      {  id : 8,
          explanation : <p><span>React, Localstorage, Redux</span>로 개발된 <span>야나두 TODO_LIST</span> 웹페이지 입니다.<br/>
                         이 프로젝트는 모바일에서 보기 적합하도록 <span>max-width : 600px 반응형 웹사이트</span>로 개발하였습니다.<br/><br/>
                         작성된 TODO_LIST data를 <span>Localstorage</span>를 이용하여, <span> CRUD기능개발 과 영구저장</span>이 가능합니다.<br/>
@@ -58,7 +80,7 @@ export default [
          gitlink : 'https://github.com/jinhee5577/TODO_LIST/tree/master/src',     
          homelink : 'https://jinhee5577.github.io/YANADO/'        
       },
-      {  id : 7,
+      {  id : 9,
          explanation : <p><br/><span>SoundBean 카페</span> 프로젝트는 제가 회사에서 디자인과 퍼블리싱을 담당한 프로젝트중 마음에 드는 결과물중 하나 입니다.<br/><br/> 
                         이 프로젝트는 유명 카페브랜드 사이트들을 참고하여 PC와 모바일에서 각각 적합한 <span>반응형 홈페이지</span> 형태로 제작되었습니다.<br/><br/>
                         각각의 메인 페이지부터 서브페이지까지 <span>디자인과 퍼블리싱 작업</span>을 담당하였고, <span>총 3페이지</span>의 카페 웹사이트 입니다.<br/>                             
